db07906a4878195caeb275605f64dd8412311c45
secdatabase <55612745+secdatabase@users.noreply.github.com>
2019-09-30 21:42:42 -0500

    Create sec_financial_statements.data_point_revision.sql

---
+++ sql/sec_financial_statements.data_point_revision.sql
@@ -0,0 +1,17 @@
+CREATE EXTERNAL TABLE sec_financial_statements.data_point_revision(
+  datapoint_id              BIGINT ,
+  accession_number_int      BIGINT ,
+  filing_date               DATE   ,
+  string_value              STRING ,
+  numeric_value             DOUBLE)
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://dataset.secdatabase.com/sec_financial_statements/parquet/data_point_revision'
+TBLPROPERTIES (
+  'has_encrypted_data'='false', 
+  'parquet.compression'='SNAPPY');
