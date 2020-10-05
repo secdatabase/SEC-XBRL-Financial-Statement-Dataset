d34e54bc457ca6af9717365624323047c3b029e9
SECDatabase.com <55612745+secdatabase@users.noreply.github.com>
2020-10-04 19:51:08 -0500

    change location to data as 2020-09-30

--- sql/sec_financial_statements.company_submission.sql
+++ sql/sec_financial_statements.company_submission.sql
@@ -33,7 +33,7 @@ STORED AS INPUTFORMAT
 OUTPUTFORMAT 
   'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
 LOCATION
-  's3://dataset.secdatabase.com/sec_financial_statements/parquet/company_submission'
+  's3://dataset.secdatabase.com/sec_financial_statements/parquet/20200930/company_submission'
 TBLPROPERTIES (
   'has_encrypted_data'='false', 
   'parquet.compression'='SNAPPY');
